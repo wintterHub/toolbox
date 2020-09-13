55f44fcdd46bb61e177c9cb47323ceede80da9fa
zhaochangjin <1505322835@qq.com>
2020-09-13 23:14:29 +0800

    添加头部及头部图片；样式调整；网络地址为空时可以添加参数

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,20 @@
 <template>
-  <el-container class="wrapper" style="border-top: solid 1px #e6e6e6;">
+  <el-container class="wrapper">
+    <el-header class="header"></el-header>
     <el-container>
       <el-aside width="250px">
-        <el-menu router :default-openeds="['1']" style="height: calc(100vh - 1px);">
+        <el-menu
+          router
+          :default-openeds="['1']"
+          style="height: calc(100vh - 60px);"
+        >
           <el-menu-item index="/urlBatchGenerat">
             <i class="el-icon-menu"></i>
             <span slot="title">链接批量生成</span>
           </el-menu-item>
         </el-menu>
       </el-aside>
-      <el-main>
+      <el-main class="main">
         <router-view></router-view>
       </el-main>
     </el-container>
@@ -17,18 +22,28 @@
 </template>
 
 <script>
-  export default {
-    data() {
-      return {}
-    },
+export default {
+  data() {
+    return {};
+  },
 
-    methods: {},
+  methods: {},
 
-    mounted() { }
-
-  }
+  mounted() {}
+};
 </script>
 
 <style scoped>
+.header {
+  z-index: 9;
+  position: relative;
+  background: #fff;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.08);
+  background-size: 100% auto;
+  background-image: url("../assets/wallpaper/PIC_20141231_154137_1C8.jpg");
+}
 
+.main {
+  padding-top: 30px;
+}
 </style>
--- src/views/UrlBatchGenerat/UrlBatchGenerat.vue
+++ src/views/UrlBatchGenerat/UrlBatchGenerat.vue
@@ -2,156 +2,223 @@
   <div>
     <el-form ref="form" :model="formData" label-width="100px">
       <el-form-item label="网络地址">
-        <el-input v-model="formData.url" placeholder="https://zhaochangjin.com/[参数1]?param=[参数2]" class="el-input-url"
-          @click.native="onUrlClick">
+        <el-input
+          v-model="formData.url"
+          placeholder="https://zhaochangjin.com/[参数1]?param=[参数2]"
+          class="el-input-url"
+          @click.native="onUrlClick"
+        >
           <el-button slot="append" @click="onAddClick">添加参数</el-button>
         </el-input>
       </el-form-item>
       <el-form-item label="参数列表" style="width: 50%;">
-        <el-table stripe :border="true" :data="formData.paramDatas" size="mini"
-          :header-cell-style="{background:'#FAFAFA',color:'#50646F','line-height':'21px'}">
+        <el-table
+          stripe
+          :border="true"
+          :data="formData.paramDatas"
+          size="mini"
+          :header-cell-style="{
+            background: '#FAFAFA',
+            color: '#50646F',
+            'line-height': '21px',
+            'border-color': '#dcdfe6'
+          }"
+        >
           <el-table-column align="center" prop="name" label="参数名">
           </el-table-column>
           <el-table-column align="center" prop="type" label="参数类型">
+            <template slot-scope="scope">
+              <span v-if="scope.row.type === 'ParamConfigNumber'">数字</span>
+              <span v-if="scope.row.type === 'ParamConfigLetter'">字母</span>
+              <span v-if="scope.row.type === 'ParamConfigTime'">时间</span>
+              <span v-if="scope.row.type === 'ParamConfigCustomize'"
+                >自定义</span
+              >
+            </template>
           </el-table-column>
           <el-table-column align="center" prop="address" label="操作">
             <template slot-scope="scope">
-              <el-button type="text" @click="onEditClick(scope.row,scope.$index)" style="padding: 0px; ">编辑</el-button>
-              <el-button type="text" @click="onDeleteClick" style="padding: 0px; ">删除</el-button>
+              <el-button
+                type="text"
+                @click="onEditClick(scope.row, scope.$index)"
+                style="padding: 0px; "
+                >编辑</el-button
+              >
+              <el-button
+                type="text"
+                @click="onDeleteClick"
+                style="padding: 0px; "
+                >删除</el-button
+              >
             </template>
           </el-table-column>
         </el-table>
-        <el-button type="primary" @click="onGeneratClick" style="margin-top: 22px;">生成地址</el-button>
-        <span style="margin-left: 22px; color:#606266;">{{generatText}}</span>
+        <el-button
+          type="primary"
+          @click="onGeneratClick"
+          style="margin-top: 22px;"
+          >生成地址</el-button
+        >
+        <span style="margin-left: 22px; color:#606266;">{{ generatText }}</span>
       </el-form-item>
       <el-form-item label="生成结果">
-        <el-input readonly type="textarea" rows="10" v-model="formData.generatContent">
+        <el-input
+          readonly
+          type="textarea"
+          rows="20"
+          v-model="formData.generatContent"
+        >
         </el-input>
       </el-form-item>
     </el-form>
-    <param-form-dialog :visible="paramFormDialogVisible" :type="paramFormDialogType" :value="currentParamData"
-      @close="paramFormDialogVisible = false" @confirm="onDialogConfirm">
+    <param-form-dialog
+      :visible="paramFormDialogVisible"
+      :type="paramFormDialogType"
+      :value="currentParamData"
+      @close="paramFormDialogVisible = false"
+      @confirm="onDialogConfirm"
+    >
     </param-form-dialog>
   </div>
 </template>
 
 <script>
-  import ParamFormDialog from './ParamFormDialog.vue'
-  export default {
-    data() {
-      return {
-        formData: {
-          paramDatas: [],
-          generatContent: ''
-        },
-        generatText: '',
-        paramFormDialogVisible: false,
-        paramFormDialogType: 'add',
-        currentParamData: {},
-        currentParamIndex: 0,
-        cursorStartPosition: 0,
-        cursorEndPosition: 0
-      }
+import ParamFormDialog from "./ParamFormDialog.vue";
+export default {
+  data() {
+    return {
+      formData: {
+        paramDatas: [],
+        generatContent: ""
+      },
+      generatText: "",
+      paramFormDialogVisible: false,
+      paramFormDialogType: "add",
+      currentParamData: {},
+      currentParamIndex: 0,
+      cursorStartPosition: 0,
+      cursorEndPosition: 0
+    };
+  },
+
+  components: {
+    "param-form-dialog": ParamFormDialog
+  },
+
+  computed: {
+    // 是否为添加参数（只读）
+    isAdd() {
+      return "add" === this.paramFormDialogType ? true : false;
     },
 
-    components: {
-      'param-form-dialog': ParamFormDialog
+    // 是否为编辑参数（只读）
+    isEdit() {
+      return "edit" === this.paramFormDialogType ? true : false;
+    }
+  },
+
+  methods: {
+    onUrlClick() {
+      // 获取光标起始位置
+      this.cursorStartPosition = document.querySelector(
+        ".el-input-url .el-input__inner"
+      ).selectionStart;
+      this.cursorEndPosition = document.querySelector(
+        ".el-input-url .el-input__inner"
+      ).selectionEnd;
     },
 
-    computed: {
-      // 是否为添加参数（只读）
-      isAdd() {
-        return 'add' === this.paramFormDialogType ? true : false
-      },
-
-      // 是否为编辑参数（只读）
-      isEdit() {
-        return 'edit' === this.paramFormDialogType ? true : false
-      }
+    onAddClick() {
+      this.paramFormDialogType = "add";
+      this.paramFormDialogVisible = true;
     },
 
-    methods: {
-      onUrlClick() {
-        // 获取光标起始位置
-        this.cursorStartPosition = document.querySelector('.el-input-url .el-input__inner').selectionStart
-        this.cursorEndPosition = document.querySelector('.el-input-url .el-input__inner').selectionEnd
-      },
+    onEditClick(row, index) {
+      this.paramFormDialogType = "edit";
+      this.paramFormDialogVisible = true;
+      this.currentParamData = row;
+      this.currentParamIndex = index;
+    },
 
-      onAddClick() {
-        this.paramFormDialogType = 'add'
-        this.paramFormDialogVisible = true
-      },
+    onDeleteClick() {},
 
-      onEditClick(row, index) {
-        this.paramFormDialogType = 'edit'
-        this.paramFormDialogVisible = true
-        this.currentParamData = row
-        this.currentParamIndex = index
-      },
-
-      onDeleteClick() { },
-
-      onGeneratClick() {
-        let result = []
-        for (let i = 0; i < this.formData.paramDatas.length; i++) {
-          if (this.formData.paramDatas[i].type === 'ParamConfigNumber') {
-            // 数字类型配置
-            if (i === 0) {
-              result = this._replaceParamConfigNumber([this.formData.url], this.formData.paramDatas[i])
-            } else {
-              result = this._replaceParamConfigNumber(result, this.formData.paramDatas[i])
-            }
+    onGeneratClick() {
+      let result = [];
+      for (let i = 0; i < this.formData.paramDatas.length; i++) {
+        // 数字类型参数替换
+        if (this.formData.paramDatas[i].type === "ParamConfigNumber") {
+          if (i === 0) {
+            // 如果是第一个参数
+            result = this._replaceParamConfigNumber(
+              [this.formData.url],
+              this.formData.paramDatas[i]
+            );
+          } else {
+            // 不是第一个参数，循环遍历result数组中的值
+            result = this._replaceParamConfigNumber(
+              result,
+              this.formData.paramDatas[i]
+            );
           }
         }
-        this.formData.generatContent = result.join('\n')
-      },
-
-      // 替换Url中数字类型的变量
-      _replaceParamConfigNumber(urlArr, paramData) {
-        let name = `[${paramData.name}]`
-        let start = paramData.paramConfig.start
-        let end = paramData.paramConfig.end
-        let action = paramData.paramConfig.action
-        let actionRange = paramData.paramConfig.actionRange
-        let result = []
-        if (action === 'up') {
-          for (let i = start; i < end; i += actionRange) {
-            urlArr.forEach(url => {
-              result.push(url.replace(name, i))
-            })
-          }
-        } else if (action === 'down') {
-          for (let i = start; i < end; i -= actionRange) {
-            urlArr.forEach(url => {
-              result.push(this.formData.url.replace(name, i))
-            })
-          }
-        }
-        return result
-      },
-
-      onDialogConfirm(val) {
-        if (this.isAdd) {
-          this.paramFormDialogVisible = false
-          this.formData.paramDatas.push(val)
-          // 拼接参数名
-          let a = this.formData.url.substring(0, this.cursorStartPosition)
-          let b = this.formData.url.substring(this.cursorEndPosition)
-          this.formData.url = `${a}[${val.name}]${b}`
-        } else if (this.isEdit) {
-          this.paramFormDialogVisible = false
-          this.formData.paramDatas.splice(this.currentParamIndex, 1, val)
-        }
       }
+      this.formData.generatContent = result.join("\n");
     },
 
-    mounted() { }
+    // 替换Url中数字类型的变量
+    _replaceParamConfigNumber(urlArr, paramData) {
+      let name = `[${paramData.name}]`;
+      let start = paramData.paramConfig.start;
+      let end = paramData.paramConfig.end;
+      let action = paramData.paramConfig.action;
+      let actionRange = paramData.paramConfig.actionRange;
+      let result = [];
+      if (action === "up") {
+        for (let i = start; i < end; i += actionRange) {
+          urlArr.forEach(url => {
+            result.push(url.replace(name, i));
+          });
+        }
+      } else if (action === "down") {
+        for (let i = start; i < end; i -= actionRange) {
+          urlArr.forEach(url => {
+            result.push(this.formData.url.replace(name, i));
+          });
+        }
+      }
+      return result;
+    },
 
-  }
+    onDialogConfirm(val) {
+      if (this.isAdd) {
+        this.paramFormDialogVisible = false;
+        this.formData.paramDatas.push(val);
+        // 拼接参数名
+        if (this.formData.url) {
+          let a = this.formData.url.substring(0, this.cursorStartPosition);
+          let b = this.formData.url.substring(this.cursorEndPosition);
+          this.formData.url = `${a}[${val.name}]${b}`;
+        } else {
+          this.formData.url = `[${val.name}]`;
+        }
+      } else if (this.isEdit) {
+        this.paramFormDialogVisible = false;
+        this.formData.paramDatas.splice(this.currentParamIndex, 1, val);
+      }
+    }
+  },
+
+  mounted() {}
+};
 </script>
 
 <style scoped>
-  .el-form-item {
-    width: 95%;
-  }
+.el-form-item {
+  width: 95%;
+}
+
+.el-table {
+  border-color: #dcdfe6;
+  border-radius: 4px;
+}
 </style>
