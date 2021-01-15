d1899f4dfe4840c248ab6d32834e7b8d26378688
zhaochangjin <1505322835@qq.com>
2021-01-15 10:55:40 +0800

    重新打包

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 module.exports = {
-  publicPath: './dist',    // 公共路径
+  publicPath: './',    // 公共路径
 
   configureWebpack: {
     plugins: [
